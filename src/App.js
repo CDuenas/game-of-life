f151e27ac69828875423ec9c3381247e97a1fb17
Christian <Chrisaduenas@gmail.com>
2020-07-22 19:27:20 -0700

    Initial commit /  ran CRA / created grid

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+const numRows = 40;
+const numCols = 40;
+
 function App() {
+  const [grid, setGrid] = useState(() => {
+    const rows = [];
+    // Create initial grid with all "Dead cells"
+    for (let i = 0; i< numRows; i++) {
+      // gives us our grid of dead cells
+      rows.push(Array.from(Array(numCols), () => 0))
+    }
+
+    return rows;
+  });
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div 
+      style = {{
+        display: "grid",
+        gridTemplateColumns: `repeat(${numCols}, 20px)`
+      }}
+    >
+      {grid.map((rows, i) =>
+        rows.map((col, k) => (
+          <div 
+            key={`${i}-${k}`}
+            style={{
+              width: 20,
+              height: 20,
+              backgroundColor: grid[i] [k] ? "blue": undefined,
+              border: "solid 1px black"
+            }}
+          />
+        ))
+      )}
     </div>
   );
 }
